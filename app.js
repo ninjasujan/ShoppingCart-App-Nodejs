e883bbdea1c5ac00e0dedd78d0616f13e4ac65a3
Sujan <Sujanbsju91@gmail.com>
2020-02-02 09:14:09 +0530

    Sequelize begin

--- app.js
+++ app.js
@@ -1,56 +1,65 @@
-const path = require('path');
 
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const errorController = require('./controllers/error');
 const User = require('./models/user');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+  User.findById('5e2714f9f5230a2020a44bad')
     .then(user => {
-      req.user = user;
+      req.user = user
+      console.log('Req.user', req.user);
       next();
     })
     .catch(err => console.log(err));
 });
+
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose
-  .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
-  )
+mongoose.connect('mongodb+srv://sujanpoojary:Jt38rgC8LeSS6UY7@cluster0-iltzc.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
-    app.listen(3000);
+    User.findOne()
+      .then(users => {
+        if(!users) {
+          console.log('No user exist in DB');
+          const user = new User({
+            name: 'Sujan',
+            email: 'sujan@gmail.com',
+            cart: []
+          });
+          user.save();
+        }
+        app.listen(4000);
+        console.log('Server listening to port 4000');
+      })
   })
-  .catch(err => {
-    console.log(err);
-  });
+  .catch(err => console.log(err)); 
+
+
+
+
+
+
+
+
+
+  
